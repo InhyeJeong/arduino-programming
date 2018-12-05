06059ad9dac8d16f64a2b7f04693f65875c49083
InhyeJeong <inhyejeong59@gmail.com>
2018-12-05 13:11:41 +0900

    miniDigitalPiano 코드 수정

--- PiezoSpeaker/miniDigtitalPiano/miniDigtitalPiano.ino
+++ PiezoSpeaker/miniDigtitalPiano/miniDigtitalPiano.ino
@@ -1,15 +1,15 @@
+// 핀번호 배열로 선언
 int pins[]= {2, 3, 4};
 int notes[] = { 262, 294, 330 };
 
 void setup() {
-   for(int i =0; i < 3 ; i ++) {
+   for(int i =0; i < sizeof(pins) ; i ++) {
       pinMode(pins[i], INPUT);
    }
-
 }
 
 void loop() {
-  for(int i = 0; i< 3; i++){
+  for(int i = 0; i< sizeof(pins); i++){
     if(digitalRead(pins[i]) == HIGH) {
         tone(8, notes[i], 100);
         delay(100);
