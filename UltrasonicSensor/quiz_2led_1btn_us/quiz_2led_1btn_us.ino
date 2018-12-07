c1e2c767b5d46c16bdd72bc7ac0c14330ba5cdc7
InhyeJeong <inhyejeong59@gmail.com>
2018-12-07 16:42:01 +0900

    ultrasonic&led&btn code editing

--- UltrasonicSensor/quiz_2led_1btn_us/quiz_2led_1btn_us.ino
+++ UltrasonicSensor/quiz_2led_1btn_us/quiz_2led_1btn_us.ino
@@ -1,78 +1,60 @@
 //버튼(5pin) / led2개(green :10pin / red:9pin) 아날로그가능
 //초음파센서( tri:2pin / ech :3pin )
-//int btn;
+
+int btn = 13;
+int red = 8;
+int green = 9;
+int trig = 2;
+int echo = 3;
+
 void setup() {
   Serial.begin(9600);
   //  ultrasonic sensor
-  pinMode(2, OUTPUT);
-  pinMode(3, INPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
   //  green led
-  pinMode(10, OUTPUT);
+  pinMode(green, OUTPUT);
   //  red led
-  pinMode(9, OUTPUT);
+  pinMode(red, OUTPUT);
   //  btn
-  pinMode(5, INPUT);
+  pinMode(btn, INPUT);
+
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(2, LOW);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);  // 10 밀리세컨드(0.01초)동안 trigger핀을 high로 하면 초음파가 나옴
+  digitalWrite(trig, LOW);
 
-  long duration = pulseIn(3, HIGH);
-
-//  if(duration == 0) {
-//    return;
-//  }
+  long duration = pulseIn(echo, HIGH);
 
   long distance = duration / 58.2 ;
 
   
   // led는 버튼이 눌린 상태에서만 점등됨
-//  btn = digitalRead(5);
-
-if(distance >=5 && distance <10){
-  Serial.println("거리가 멀어짐");
-  if(digitalRead(5) == HIGH){
-    Serial.println("btn눌림");
-         //초록색 점등
-      digitalWrite(10, HIGH);
-      digitalWrite(9, LOW);
+  if(digitalRead(btn) == HIGH){
+    Serial.println("btn pushed");
+    
+    if(distance >=5 && distance <10){
+      //초록색 점등
+      digitalWrite(green, HIGH);
+      digitalWrite(red, LOW);
        Serial.println("거리 5이상 10 미만 : green 점등");
     } else {
       // 빨간색 점등
-      digitalWrite(9, HIGH);
-      digitalWrite(10, LOW);
+      digitalWrite(red, HIGH);
+      digitalWrite(green, LOW);
        Serial.println("거리가 멀어짐 red 점등");
     }
- 
-} else {
-   digitalWrite(10, LOW);
-    digitalWrite(9, LOW);
-}
- Serial.println(distance);
-delay(100);
-
-//  if(digitalRead(5) == HIGH){
-//    Serial.println("버튼이 눌렸음");
-//    
-//    if(distance >=5 && distance <10){
-//      //초록색 점등
-//      digitalWrite(10, HIGH);
-//      digitalWrite(9, LOW);
-//       Serial.println("거리 5이상 10 미만 : green 점등");
-//    } else {
-//      // 빨간색 점등
-//      digitalWrite(9, HIGH);
-//      digitalWrite(10, LOW);
-//       Serial.println("거리가 멀어짐 red 점등");
-//    }
-//    
-//  } else {
-//    digitalWrite(10, LOW);
-//    digitalWrite(9, LOW);
-//  }
-//  Serial.println(distance);
-//  delay(100);
+    
+  } else {
+    Serial.println("btn unpushed");
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
+  }
+  Serial.println("distance : ");
+  Serial.println(distance);
+  Serial.println("------------------------------------------------------");
+  delay(100);
 
 }
